ac4b846f55a120ac174f8e3dde0343e39627cb91
Максим Абрамович < >
2017-03-19 18:53:35 +0300

    remove files

--- Specflow.Demonstration_/Button.cs
+++
@@ -1,9 +0,0 @@
-using Specflow.Demonstration.CalculatorApp;
-
-namespace Specflow.Demonstration.Steps
-{
-    public class Button
-    {
-        public CalculatorOperation Operation { get; set; }
-    }
-}
