d4f649ec158ab0ae84eeea2705b3dcc8d00f5572
Максим Абрамович < >
2017-03-19 18:50:17 +0300

    Another commit

---
+++ Specflow.Demonstration_/Button.cs
@@ -0,0 +1,9 @@
+using Specflow.Demonstration.CalculatorApp;
+
+namespace Specflow.Demonstration.Steps
+{
+    public class Button
+    {
+        public CalculatorOperation Operation { get; set; }
+    }
+}
---
+++ Specflow.Demonstration_/Hooks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Specflow.Demonstration
+{
+    [Binding]
+    public static class Hooks
+    {
+        [BeforeFeature]
+        public static void BeforeFeatureHook()
+        {
+            Console.WriteLine("BeforeFeature");
+        }
+
+        [BeforeScenario]
+        public static void BeforeScenarioHook()
+        {
+            Console.WriteLine("BeforeScenario");
+        }
+
+        [AfterFeature]
+        public static void AfterFeatureHook()
+        {
+            Console.WriteLine("AfterFeature");
+        }
+
+        [AfterScenario]
+        public static void AfterScenarioHook()
+        {
+            Console.WriteLine("AfterScenario");
+        }
+    }
+}
